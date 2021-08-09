27efbfb550a98dec8e1db524aee91a255cd264b2
Reza Rahman <reza_rahman@lycos.com>
2021-08-09 19:05:58 -0400

    Fix lookup.

--- hello-world-ejb/src/main/java/helloworld/HelloWorldHome.java
+++ hello-world-ejb/src/main/java/helloworld/HelloWorldHome.java
@@ -6,5 +6,5 @@ import javax.ejb.CreateException;
 import javax.ejb.EJBHome;
 
 public interface HelloWorldHome extends EJBHome {
-	HelloWorldRemote create() throws CreateException, RemoteException;
+  HelloWorldRemote create() throws CreateException, RemoteException;
 }
